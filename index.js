400ebf041bc33c1e2e3eb4c9943f85c5180f725a
Alexander Craggs <popey@debenclipper.com>
2017-08-06 21:16:24 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const Manager = require('./modules/Manager')
+
+global.db = require('sqlite')
+const express = require('express')
+const bodyParser = require('body-parser')
+const shortid = require('shortid')
+const app = express()
+
+const SERVER_IP = '127.0.0.1'
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+if (process.env.NODE_ENV === 'production' && !process.env.KEY) throw new Error('Error, universal key not given.')
+
+app.get('/list', async (req, res) => { res.json(await Manager.listServers()) })
+app.get('/info/:node', async (req, res) => { res.json(await Manager.infoServer(req.params.node)) })
+app.post('/create', async (req, res) => { res.json(await Manager.createServer(req.params.node, req.body)) })
+app.delete('/delete/:node', async (req, res) => { res.json(await Manager.deleteServer(req.params.node)) })
+app.post('/action/:node', async (req, res) => { res.json(await Manager.actionServer(req.params.server, req.body)) })
+
+// db { id, type, server_ip, server_port, running }
+
+;(async () => {
+  await db.open('./db/sqlite.db')
+  await db.all('CREATE TABLE IF NOT EXISTS instances (id TEXT, type TEXT, server_ip TEXT, server_port TEXT, running BOOLEAN);')
+  app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+  })
+})()
---
+++ modules/Manager.js
@@ -0,0 +1,42 @@
+function listServers() {
+  return new Promise(async (resolve) => {
+    const info = await db.all('SELECT * FROM instances')
+    resolve({
+      time: +new Date(),
+      status: 200,
+      message: 'Listed Available Servers',
+      data: info
+    })
+  })
+}
+
+function infoServer(node) {
+  return new Promise(async (resolve) => {
+    const info = await db.all('SELECT * FROM instances WHERE id=?', node)
+    resolve({
+      time: +new Date(),
+      status: 200,
+      message: 'Server Query Successful',
+      data: info
+    })
+  })
+}
+
+function createServer(node, settings) {
+  return new Promise(async (resolve) => {
+    const uuid = shortid.generate()
+    const server_port = Math.ceil(Math.random() * 20000)
+    await db.all('INSERT INTO instances (id, type, server_ip, server_port, running) VALUES (?, ?, ?, ?, ?);', uuid, settings.type, SERVER_IP, server_port, true)
+    resolve(await infoServer(uuid))
+  })
+}
+
+function deleteServer(node) {
+  return db.all('DELETE from instances WHERE id=?', node)
+}
+
+function actionServer(node, settings) {
+  return {}
+}
+
+module.exports = { listServers, infoServer, createServer, deleteServer, actionServer }
